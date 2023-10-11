46d9330435d4aca32e5917687d7a5997b4defbee
Adekurp <Adekurp@gmail.com>
2023-10-11 17:38:22 +0700

    create listings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Profile from './pages/profile';
+import Profile from './pages/Profile';
 import About from './pages/about';
 import Home from './pages/home';
 import SignIn from './pages/signIn';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
 
 export default function App() {
   return <BrowserRouter>
@@ -17,6 +18,7 @@ export default function App() {
     <Route path="/about" element={<About />}/>
     <Route  element={<PrivateRoute />}>
       <Route path="/profile" element={<Profile />}/>
+      <Route path='/create-listing' element={<CreateListing />} />
     </Route>
   </Routes>
   </BrowserRouter>;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,6 +7,7 @@ import {deleteUserFailure, deleteUserStart, deleteUserSuccess,
 updateUserFailure, updateUserStart, updateUserSuccess,
 signOutUserFailure, signOutUserStart, signOutUserSuccess} from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const {currentUser, loading, error} = useSelector((state) => state.user);
@@ -130,6 +131,9 @@ export default function Profile() {
         <button disabled={loading} className='bg-slate-700 text-white rounded-xl p-3 uppercase hover:opacity-95 disabled:opacity-80'>
             { loading ? 'Loading...' : 'Update'}
         </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
       <div className='flex justify-between mt-4'>
         <span onClick={handleDeleteUser} className='text-red-600 cursor-pointer'>Delete account</span>
